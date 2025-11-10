4314100d0b01d056754a1f88c7ebeb3a9519f197
a-ilawy <mahmed20156947@gmail.com>
2025-11-10 22:57:48 +0200

    feat(products): add bet seller filters and to selling api

--- project/orders/tests/factories/order_factory.py
+++ project/orders/tests/factories/order_factory.py
@@ -1,11 +1,8 @@
 import factory
 from faker import Faker
-from django.contrib.auth import get_user_model
-from products.models.product import Product
 from orders.models.order import Order, OrderItem
 from accounts.tests.factories.user_factory import UserFactory
-from products.models.category import Category
-from products.models.brand import Brand
+from products.tests.factories.products_factory import ProductFactory
 
 fake = Faker()
 
@@ -13,31 +10,6 @@ fake = Faker()
 class AdminFactory(UserFactory):
     user_type = "admin"
     is_staff = True
-
-
-
-class CategoryFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Category
-
-    name = factory.LazyAttribute(lambda _: fake.word())
-
-
-class BrandFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Brand
-
-    name = factory.LazyAttribute(lambda _: fake.word())
-    
-class ProductFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Product
-
-    name = factory.LazyAttribute(lambda _: fake.word())
-    price = factory.LazyAttribute(lambda _: fake.pydecimal(left_digits=3, right_digits=2, positive=True))
-    stock = factory.LazyAttribute(lambda _: fake.random_int(min=5, max=20))
-    category = factory.SubFactory(CategoryFactory)
-    brand = factory.SubFactory(BrandFactory)
 
 
 class OrderFactory(factory.django.DjangoModelFactory):
